02ee5f11124531caa06be24ed8ab90aa04e9c747
Kaio Sergio <kaioscsouza@gmail.com>
2024-06-03 15:46:08 -0300

    Add: All API

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import routes from "./routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+routes(app);
+
+app.listen(3001);
+
+console.log("Servidor iniciou");
---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import { create, get, getId, update, remove} from "../controller/user.controller";
+
+const userRoutes = app => {
+    app.post("/user", create);
+    app.get("/user", get);
+    app.get("/user/:id", getId);
+    app.put("/user/:id", update);
+    app.delete("/user/:id", remove);
+};
+
+export default userRoutes;
